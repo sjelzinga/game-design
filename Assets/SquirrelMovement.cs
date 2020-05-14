5ca25a3f83669be11652be469983f440bf6e5570
sjelzinga <sje.elzinga@gmail.com>
2020-05-14 13:20:38 +0200

    Add health bar

--- Assets/SquirrelMovement.cs
+++ Assets/SquirrelMovement.cs
@@ -10,11 +10,17 @@ public class SquirrelMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -27,10 +33,23 @@ public class SquirrelMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // replace by a timer
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DecreaseHealth(20);
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void DecreaseHealth(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
