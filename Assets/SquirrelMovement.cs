9798366b83bbb64b8433fc8602b7bd9169e72ac1
Moonglow87 <koopmans.michael@gmail.com>
2020-05-27 23:38:36 +0200

    MouseMovement

--- Assets/SquirrelMovement.cs
+++ Assets/SquirrelMovement.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SquirrelMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 0.1f;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -15,24 +16,45 @@ public class SquirrelMovement : MonoBehaviour
 
     public HealthBar healthBar;
 
-    Vector2 movement;
+    private Vector3 min, max;
+    Vector3 mousePosition;
+    Vector2 position = new Vector2(0f, 0f);
+
+    public bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         healthBar.SetHealth(maxHealth);
+        max.x = 8f;
+        max.y = 3.45f;
+        min.x = 8f;
+        min.y = 3.45f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        GetMousePosition();
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (isMoving)
+        {
+            float distance = Vector2.Distance(transform.position, mousePosition);
+
+            if (distance >= 0)
+            {
+                position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed);
+                animator.SetFloat("Horizontal", position.x - transform.position.x);
+                animator.SetFloat("Vertical", position.y - transform.position.y);
+                animator.SetFloat("Speed", position.sqrMagnitude);
+            }
+            else
+                isMoving = false;
+                animator.SetFloat("Speed", position.sqrMagnitude);
+
+        }
 
         // replace by a timer
         if (Input.GetKeyDown(KeyCode.Space))
@@ -41,9 +63,32 @@ public class SquirrelMovement : MonoBehaviour
         }
     }
 
+
+    void GetMousePosition()
+    {
+        if (Input.GetMouseButtonDown(0) && PositionIsInRange())
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            isMoving = true;
+        }
+        else if(!Input.GetMouseButton(0))
+            isMoving = false;
+    }
+
+    private bool PositionIsInRange()
+    {
+        return true;
+        //mousePosition = Input.mousePosition;
+        //mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //if (mousePosition.x > min.x && mousePosition.y > min.y && mousePosition.x < max.x && mousePosition.y < max.y)
+        //    return true;
+        //return false;
+    }
+
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(position);
     }
 
     void DecreaseHealth(int damage)
@@ -53,3 +98,4 @@ public class SquirrelMovement : MonoBehaviour
         healthBar.SetHealth(currentHealth);
     }
 }
+
